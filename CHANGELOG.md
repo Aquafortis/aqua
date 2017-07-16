93567b1551d51d6181b26590c3bc4707f0093427
Aquafortis <ian@320up.com.au>
2017-07-16 18:45:29 +1000

    Updated to Version 1.6.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## Version 1.6.2
+## 2017-07-16
+- Tidy up HTML.
+- Using Bludit version numbers.
+
 ## Version 1.1
 ## 2017-07-16
 - Fixed CSS link. Added Cover Image.
--- php/post.php
+++ php/post.php
@@ -1,34 +1,34 @@
 <h1 class="subhead"><?php echo $Language->get('Post') ?></h1>
 
-<section class="post">
+<article class="post">
 
-    <!-- Plugins Post Begin -->
-    <?php Theme::plugins('postBegin') ?>
+  <!-- Plugins Post Begin -->
+  <?php Theme::plugins('postBegin') ?>
 
-    <!-- Post header -->
-    <header class="post-header">
+  <!-- Post header -->
+  <header class="post-header">
 
-        <!-- Post title -->
-        <h2 class="post-title">
-            <a href="<?php echo $Post->permalink() ?>"><?php echo $Post->title() ?></a>
-        </h2>
+    <!-- Post title -->
+    <h2 class="post-title">
+      <a href="<?php echo $Post->permalink() ?>"><?php echo $Post->title() ?></a>
+    </h2>
 
-        <!-- Post date and author -->
-        <div class="post-meta">
-            <span class="date"><?php echo $Post->date() ?></span>
-            <span class="author">
-                <?php
-                    echo $Language->get('Posted By').' ';
+    <!-- Post date and author -->
+    <div class="post-meta">
+      <span class="date"><?php echo $Post->date() ?></span>
+      <span class="author">
+      <?php
+        echo $Language->get('Posted By').' ';
 
-                    if( Text::isNotEmpty($Post->user('firstName')) || Text::isNotEmpty($Post->user('lastName')) ) {
-                        echo $Post->user('firstName').' '.$Post->user('lastName');
-                    }
-                    else {
-                        echo $Post->user('username');
-                    }
-                ?>
-            </span>
-        </div>
+          if( Text::isNotEmpty($Post->user('firstName')) || Text::isNotEmpty($Post->user('lastName')) ) {
+            echo $Post->user('firstName').' '.$Post->user('lastName');
+          }
+          else {
+            echo $Post->user('username');
+          }
+        ?>
+        </span>
+      </div>
 
     </header>
 
@@ -41,10 +41,10 @@
 
     <!-- Post content -->
     <div class="post-content">
-        <?php echo $Post->content() ?>
+      <?php echo $Post->content() ?>
     </div>
 
     <!-- Plugins Post End -->
     <?php Theme::plugins('postEnd') ?>
 
-</section>
+</article>
